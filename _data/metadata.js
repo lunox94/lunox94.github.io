32db85eb452e8bead2c8e7bdbfd0751e3d0e2336
lunox94 <gmrlunox94@gmail.com>
2024-10-13 12:17:25 +0000

    Migrate to eleventy-base-blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Lunox Dev Blog",
+	url: "https://lunox94.github.io/",
+	language: "en",
+	description: "I am writing about my experiences as a software developer.",
+	author: {
+		name: "Gerardo Martinez",
+		email: "gmlunox.dev@gmail.com",
+		url: "https://lunox94.github.io/about/"
+	}
+}
